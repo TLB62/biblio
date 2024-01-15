3651fa39fbcde3c82a9bf3fa68e8f78fc2d5b0df
thomas.lebaronbzh@gmail.com <thomas.lebaronbzh@gmail.com>
2024-01-15 10:35:10 +0100

    update2 index.php

--- www/index.php
+++ www/index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- based css bootstrap by bootswatch -->
     <link rel="stylesheet" href="https://bootswatch.com/5/sketchy/bootstrap.min.css">
-    <title>Bibliothèque</title>
+    <title>Biblio2</title>
 </head>
 
 <body>
