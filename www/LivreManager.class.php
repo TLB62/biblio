68e7ca580cb1871961d8adb7eeaf47e7e8d87ca4
thomas.lebaronbzh@gmail.com <thomas.lebaronbzh@gmail.com>
2024-01-16 14:21:42 +0100

    update3 index.php

---
+++ www/LivreManager.class.php
@@ -0,0 +1,34 @@
+<?php
+require_once "ConnexionManager.class.php";
+
+class LivreManager extends ConnexionManager
+{
+    private array $livres;
+
+    public function ajouterLivre($nouveauLivre) 
+    {
+        $this->livres[] = $nouveauLivre;
+    }
+
+    public function chargementLivres() 
+    {
+        $connexion = $this->getConnexionBdd();
+        $req = $connexion->prepare("SELECT * FROM livre limit 0, 10");
+        $req->execute();
+        $livresImportes = $req->fetchALL(PDO::FETCH_ASSOC);
+        $req->closeCursor();
+        
+        foreach ($livresImportes as $livre) {
+            $nouveauLivre = new Livre($livre['id_livre'], $livre['titre'], $livre['image'], $livre['nb_pages']);
+            $this->ajouterLivre($nouveauLivre);
+        }
+    }
+    /**
+     * Get the value of livres
+     *
+     * @return array
+     */
+    public function getLivres(): array {
+        return $this->livres;
+    }
+}
---
+++ www/livres.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once "Livre.class.php";
+require_once "LivreManager.class.php";
+
+$livreManager = new LivreManager;
+$livreManager->chargementLivres();
+
+
+?>
+
+<?php ob_start() ?>
+
+<table class="table table-center">
+    <tr class="table-dark">
+        <th>Image</th>
+        <th>Titre</th>
+        <th>Nombre de pages</th>
+        <th colspan="2">Actions</th>
+    </tr>
+    <?php 
+    $livresenCours = $livreManager->getLivres();
+    foreach($livresenCours as $livre) : ?>
+    <tr>
+        <td class="align-middle"><img src="public/images/<?php echo $livre->getImage(); ?>" height="60px" alt="Livre pour <?php echo $livre->getTitre(); ?>"></td>
+        <td class="align-middle"><?php echo $livre->getTitre(); ?></td>
+        <td class="align-middle"><?php echo $livre->getNbPages(); ?></td>
+        <td class="align-middle"><a href="#" class="btn btn-warning">Modifier</a></td>
+        <td class="align-middle"><a href="#" class="btn btn-danger">Supprimer</a></td>
+    </tr>
+    <?php endforeach; ?>
+</table>
+<a href="#" class="btn btn-success d-block">Ajouter</a>
+
+<?php
+$titre = " Livres de Thomas";
+$content = ob_get_clean();
+require_once "template.php";
